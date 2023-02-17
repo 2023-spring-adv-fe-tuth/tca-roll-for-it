35fbe863afbdfb230bf1bcfcd770fdbf0101d773
tsteelematc <tsteele@madisoncollege.edu>
2023-02-17 09:50:59 -0600

    Playing with some more stats...
    Hmm, hope I like daisyUI ? ? ?

--- src/Home.tsx
+++ src/Home.tsx
@@ -6,18 +6,34 @@ export const Home = () => {
             >
                 Home
             </h2>
-
+            <br />
+            <br />
             <div className="stats shadow">
-  
                 <div className="stat">
                     <div className="stat-title">Total Page Views</div>
                     <div className="stat-value">89,400</div>
                     <div className="stat-desc">21% more than last month</div>
                 </div>
-                
             </div>
-
-            
+            <br />
+            <br />
+            <div className="stats shadow">
+                <div className="stat place-items-center">
+                    <div className="stat-title">Downloads</div>
+                    <div className="stat-value">31K</div>
+                    <div className="stat-desc">From January 1st to February 1st</div>
+                </div>
+                <div className="stat place-items-center">
+                    <div className="stat-title">Users</div>
+                    <div className="stat-value text-secondary">4,200</div>
+                    <div className="stat-desc text-secondary">↗︎ 40 (2%)</div>
+                </div>
+                <div className="stat place-items-center">
+                    <div className="stat-title">New Registers</div>
+                    <div className="stat-value">1,200</div>
+                    <div className="stat-desc">↘︎ 90 (14%)</div>
+                </div>
+            </div>
         </>
     );
 };
