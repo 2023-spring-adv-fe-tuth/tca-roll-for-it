c27124ca172a193bef902b5757770a8ec5726907
tsteelematc <tsteele@madisoncollege.edu>
2023-02-17 09:06:32 -0600

    Routes working'ish...
    Still need to figure out how to pass props with latest react-router-dom ? ? ?
    
    And what this loader: prop is ? ? ?

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,43 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+
 import { Home } from './Home';
+import { Setup } from './Setup';
+import { Play } from './Play';
+
+import {
+  createHashRouter
+  , RouterProvider
+} from "react-router-dom";
+
+const router = createHashRouter([
+  {
+    path: "/",
+    element: <Home />,
+    // loader: rootLoader,
+    // children: [
+    //   {
+    //     path: "team",
+    //     element: <Team />,
+    //     loader: teamLoader,
+    //   },
+    // ],
+  }
+  , {
+    path: "/setup"
+    , element: <Setup />
+  }
+  , {
+    path: "/play"
+    , element: <Play />
+  }
+]);
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <RouterProvider router={router} />
     </div>
   );
 }
