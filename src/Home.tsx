da8f1e41a7b2c5f5be47c1c20e5f220c74f0a4dc
tsteelematc <tsteele@madisoncollege.edu>
2023-02-17 09:48:55 -0600

    DaisyUI working'ish ! ! !
    npm i daisyui
    
    And hacked in a stat component...

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,9 +1,23 @@
 export const Home = () => {
     return (
-        <h2
-            className="text-3xl"
-        >
-            Home
-        </h2>
+        <>
+            <h2
+                className="text-3xl"
+            >
+                Home
+            </h2>
+
+            <div className="stats shadow">
+  
+                <div className="stat">
+                    <div className="stat-title">Total Page Views</div>
+                    <div className="stat-value">89,400</div>
+                    <div className="stat-desc">21% more than last month</div>
+                </div>
+                
+            </div>
+
+            
+        </>
     );
 };
