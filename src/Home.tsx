02577b5ac2c1d41be662fbcdbb2d2d378f241342
tsteelematc <tsteele@madisoncollege.edu>
2023-02-18 20:50:00 -0600

    Title items-center...

--- src/Home.tsx
+++ src/Home.tsx
@@ -14,7 +14,7 @@ export const Home = () => {
                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-5 h-5 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
                 </button>
             </div>
-            <div className="flex flex-col mx-3">
+            <div className="flex flex-col items-start mx-3">
                 <h1 className="text-2xl font-bold">Roll for It</h1>
                 <h2 className="text-sm uppercase font-semi-bold -mt-1">Companion App</h2>
             </div>
