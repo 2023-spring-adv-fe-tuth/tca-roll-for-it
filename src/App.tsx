5481782e91d58a742edf84edcf9d31972d8891c4
Tom Steele <tsteele@madisoncollege.edu>
2023-02-23 09:25:37 -0600

    Less emphasis on navbar title...
    A pixel perfect horror story...
    
    From my MacBook Pro : - O

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ function App() {
     ? "Play" 
     : location.pathname == "/setup"
       ? "Setup"
-      : "Roll for It"
+      : "Roll for It Companion App"
   ;
 
   return (
@@ -30,8 +30,8 @@ function App() {
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-5 h-5 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
           </button>
         </div>
-        <div className="flex flex-col items-start mx-3">
-          <h1 className="text-3xl font-bold">{title}</h1>
+        <div className="flex flex-col items-start">
+          <h1 className="text-sm font-light">{title}</h1>
           {/* {
             location.pathname == "/" && <h2 className="text-sm uppercase font-semi-bold -mt-1">Companion App</h2>
           } */}
