aeb1438aa0cded05b78bb2539afb191eb943d8e7
Tom Steele <tsteele@madisoncollege.edu>
2023-02-25 08:09:43 -0600

    Proper input of player name...
    Next, real function with validation ! ! !

--- src/Setup.tsx
+++ src/Setup.tsx
@@ -24,47 +24,66 @@ export const Setup: FC<SetupProps> = ({
         , checked: false
     })))    
 
+    const [newPlayerName, setNewPlayerName] = useState("");
+
     return (
         <div
             className="flex flex-col p-1"
         >
-            <h3
-                className="text-2xl text-left"
-            >
-                Choose players...
-            </h3>
-            <button 
-                className="btn btn-link capitalize"
-                onClick={() => setChosenPlayers([
-                    ...chosenPlayers 
-                    , {
-                        name: "Dummy Player with a really really long name, really long, yeah"
-                        , checked: true
-                    }
-                ])}
-            >
-                Add Dummy Player
-            </button>
-            <ul>
-                {chosenPlayers.map(x => (
-                    <div className="form-control1 mt-5">
-                        <label className="label1 flex ml-10">
-                            <input 
-                                type="checkbox" 
-                                checked={x.checked} 
-                                className="checkbox checkbox-primary" 
-                                onChange={() => setChosenPlayers([
-                                    ...chosenPlayers.map(y => ({
-                                        ...y
-                                        , checked: x.name == y.name ? !y.checked : y.checked 
-                                    }))
-                                ])}
-                            />
-                            <span className="label-text text-xl ml-3 capitalize">{x.name}</span> 
-                        </label>
+                <div
+                    className="flex"
+                >
+                    <div className="card w-96 bg-base-100 shadow-xl grow">
+                        <div className="card-body">
+                            <h2 className="card-title">Choose or add players...</h2>
+                            <div className="form-control">
+                                <div className="input-group mt-3">
+                                    <input 
+                                        type="text" 
+                                        placeholder="New Player Name" 
+                                        className="input input-bordered grow" 
+                                        value={newPlayerName}
+                                        onChange={(e) => setNewPlayerName(e.target.value)}
+                                    />
+                                    <button 
+                                        className="btn btn-square btn-primary"
+                                        onClick={() => setChosenPlayers([
+                                            ...chosenPlayers 
+                                            , {
+                                                name: newPlayerName
+                                                , checked: true
+                                            }
+                                        ])}
+                                    >
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" viewBox="0 0 16 16">
+                                            <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
+                                        </svg>
+                                    </button>
+                                </div>
+                            </div>                            
+                            <ul>
+                                {chosenPlayers.map(x => (
+                                    <div className="form-control1 mt-5">
+                                        <label className="label1 flex">
+                                            <input 
+                                                type="checkbox" 
+                                                checked={x.checked} 
+                                                className="checkbox checkbox-primary" 
+                                                onChange={() => setChosenPlayers([
+                                                    ...chosenPlayers.map(y => ({
+                                                        ...y
+                                                        , checked: x.name == y.name ? !y.checked : y.checked 
+                                                    }))
+                                                ])}
+                                            />
+                                            <span className="label-text text-xl ml-3 capitalize">{x.name}</span> 
+                                        </label>
+                                    </div>
+                                ))}
+                            </ul>
+                        </div>
                     </div>
-                ))}
-            </ul>
+                </div>
             <button 
                 className="btn btn-lg btn-primary capitalize mt-5"
                 onClick={() => nav("/play")}
