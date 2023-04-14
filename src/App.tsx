1c370a8c4d213aa17ce8a18fa1b8961b20fd9845
tsteelematc <tsteele@madisoncollege.edu>
2023-04-13 19:38:46 -0500

    Hmm...

--- src/App.tsx
+++ src/App.tsx
@@ -443,7 +443,7 @@ function App() {
 				
 				setSettings(s ?? {darkMode: false, email: ""});
 				setEmailOnModal(s?.email ?? "");
-				setShowEmailModal(s?.email.length == 0);
+				setShowEmailModal((s?.email ?? "").length == 0);
 			};
 
 			loadSettings();
@@ -551,10 +551,10 @@ function App() {
 				{
 					title == "Roll for It" && (
 						<div 
-							className='flex-none mr-3 mt-3'
+							className='flex-none mr-5'
 							onClick={() => setShowEmailModal(true)}
 						>
-							<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" viewBox="0 0 24 24">
+							<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 16 16">
 								<path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0Zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4Zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10Z"/>
 							</svg>					
 						</div>
@@ -631,7 +631,9 @@ function App() {
 							className="btn btn-primary"
 							onClick={updateEmail}
 						>
-							Update
+							<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" viewBox="0 0 16 16">
+								<path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425a.247.247 0 0 1 .02-.022Z"/>
+							</svg>
 						</button>
 					</div>
 				</div>
